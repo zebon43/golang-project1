6048ab83e3935311731a6d4617d5c90213162f23
Ankit Trivedi <48177723+zebon43@users.noreply.github.com>
2020-02-05 17:38:23 +0530

    Added channels
    Added channels

---
+++ Projects/14_Channels.go
@@ -0,0 +1,17 @@
+package main
+
+import (  
+    "fmt"
+)
+
+func testchannels(done chan bool) {  
+    fmt.Println("Inside Go Routine")
+    done <- true //Send data to channel
+}
+func main() {  
+    status := make(chan bool) // Declare a channel variable
+    go testchannels(status)  //Invoke the function using go routine
+    x:= <-done  // Get data from the channel
+    fmt.Println("Data from Channel:",x)
+	fmt.Println("Main function Ends")
+}
